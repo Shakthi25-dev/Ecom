685369e192c81ff773a5e0bce2e4b91f667ded5e
Shakthi <shakthiraja2k4@gmail.com>
2025-06-19 16:21:25 +0530

    initial commit

--- backend/controller/Product.js
+++ backend/controller/Product.js
@@ -1,11 +1,10 @@
 import { Product } from "../models/Product.js";
+import { rm } from "fs";
 
 //Add new product
 export const createProduct = async (req, res) => {
     try {
-        console.log("req.body:", req.body);  
-        console.log("req.file:", req.file);   
-
+      
         if (req.login.role != "admin") {
             return res.status(403).json({
                 message: "Unauthorized Access",
@@ -41,3 +40,70 @@ export const createProduct = async (req, res) => {
         });
     }
 };
+
+//Fetch all products
+export const fetchAllProduct = async (req, res) => {
+    try {
+        const product = await Product.find();
+        return res.status(200).json({
+            message: "List of products", product
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
+
+};
+
+//Fetch single product
+export const fetchSingleProduct = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const product = await Product.findById(id);
+        return res.status(200).json({
+            message: "Product Details", product
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
+
+};
+
+//Delete the product
+export const deleteProduct = async (req, res) => {
+    try{
+        if (req.login.role != "admin") {
+                return res.status(403).json({
+                    message: "Unauthorized Access",
+                });
+        
+        }  
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(403).json({
+                message: "Inavalid Product Details",
+            });
+        }
+
+        //Removing the image
+        rm(product.image, () => {
+            console.log("Image Deleted");
+        });
+
+        await product.deleteOne();
+        return res.json({
+            message: "Product details successfully deleted",
+        });
+
+
+    } catch (error){
+          return res.status(500).json({
+            message: error.message,
+        });
+    }
+}
+
+
--- backend/routes/Product.js
+++ backend/routes/Product.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { isAuth } from "../middleware/isAuth.js";
 import { uploadFiles } from "../models/multer.js";
-import { createProduct } from "../controller/Product.js";
+import { createProduct, fetchAllProduct, fetchSingleProduct, deleteProduct } from "../controller/Product.js";
 
 const router = express.Router();
 router.post("/product/new", uploadFiles, isAuth, createProduct);
+router.get("/product/all-products", fetchAllProduct);
+router.get("/product/single/:id", fetchSingleProduct);
+router.delete("/product/:id", isAuth, deleteProduct);
+
+
 export default router;
